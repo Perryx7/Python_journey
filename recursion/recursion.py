6023942c5e456fd2e3326086d7f8a21859854c40
Perryx7 <jacquesperry30@gmail.com>
2025-09-07 23:51:58 +0100

    recursion stape

---
+++ recursion/recursion.py
@@ -0,0 +1,13 @@
+# a recursion happend in python when a function call itself
+
+def add_one(num) : 
+    if (num >= 9 ) :
+        return num + 1
+    
+    total = num + 1
+    print(total)
+
+    return add_one(total)
+
+mynewtotal = add_one(0)
+print(mynewtotal)
