e60d4dd96dac46a5cb178a7eb70cae0e0f0d3254
Perryx7 <jacquesperry30@gmail.com>
2025-09-08 19:10:08 +0100

    initialise advanced function exercice

---
+++ function/real_life_ex/rl5.py
@@ -0,0 +1,41 @@
+# snack bar and cinema management
+
+# Scenario:
+
+# You are building a system to manage a small cinema hall with ticket booking and a snack bar.
+
+# Requirements / Tasks:
+
+# Movie Management
+
+# Store movies in dictionary {movie_title: {seats, price}}.
+
+# Ticket Booking
+
+# Users can book seats (update available seats).
+
+# Prevent overbooking.
+
+# Snack Bar
+
+# Store menu in dictionary {item: price}.
+
+# Users can order multiple snacks → add to total bill.
+
+# Discounts
+
+# Combo discount: If ticket + snacks > $30 → 15% discount.
+
+# Reports
+
+# Most watched movie.
+
+# Most sold snack.
+
+# Total revenue.
+
+# Advanced
+
+# Track which users watched which movies (dictionary of lists).
+
+# Recommend movies: “Users who watched X also watched Y”.
