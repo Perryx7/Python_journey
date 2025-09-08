d7ea908d735a838698889c3430911dff06044478
Perryx7 <jacquesperry30@gmail.com>
2025-09-08 19:07:34 +0100

    add another basics challenges

---
+++ ex_basics/r_p_7.py
@@ -0,0 +1,5 @@
+# password generator
+# Ask the user to create a password and check that it contains at least:
+# o	8 characters
+# o	one uppercase letter
+# o	one digit
---
+++ ex_basics/r_p_8.py
@@ -0,0 +1,4 @@
+# Banka acoount simulator
+# Ask the user for an initial balance.
+#  Allow them to perform 3 simple operations: 
+# deposit, withdraw, check balance.
