dea2bb882d82ce197117f40b2b72da8c25484682
Perryx7 <jacquesperry30@gmail.com>
2025-09-09 01:42:39 +0100

    solution of basics problem

--- ex_basics/r_p_7.py
+++ ex_basics/r_p_7.py
@@ -3,3 +3,38 @@
 # o	8 characters
 # o	one uppercase letter
 # o	one digit
+
+
+
+
+
+
+def checkpassword(password_input):
+  #stock the lenght of the password 
+  password_len = len(password_input)
+  #chzck the validity of the password
+  if password_len >= 8 and any(char.isdigit() for char in password_input) and any(char.isupper() for char in password_input):
+    print("mot de passe solide")
+  else:
+    print("wrong password")
+    #call itselt untill the password is good
+    newpassword = input("try another password")
+    checkpassword(newpassword)
+
+
+
+password_input =  input(" enter your password ")
+checkpassword(password_input)
+
+
+
+
+
+
+    
+
+
+
+    
+   
+
--- ex_basics/r_p_8.py
+++ ex_basics/r_p_8.py
@@ -2,3 +2,62 @@
 # Ask the user for an initial balance.
 #  Allow them to perform 3 simple operations: 
 # deposit, withdraw, check balance.
+
+
+def deposit(balance , amount) :
+ #initialise the balance with the cureent balance + the current amount you want to make a deposit
+ balance += amount 
+ 
+ print ("you make a deposit off " + str(amount) + " " + "\n you new balance is " + str(balance))
+
+#return the new balance
+ return balance
+
+
+def withdraw(balance , amount) :
+ #initialise the balance with the cureent balance + the current amount you want to make a withdraw
+ balance -= amount 
+ 
+ print ("you make a withdraw off " + str(amount) + " " + "\n you new balance is " + str(balance))
+
+#return the new balance
+ return balance
+
+
+
+
+
+
+
+balance = int(input ("enter your initial balance "))
+
+while True:
+    action = input("  1 for deposit , 2 for withdraw, 3 for check, 4 for exist ").strip().lower()
+
+    # deposit action
+    if action == "1" :
+     amount = int(input("enter the amount of your deposit   "))
+     if amount < 0 :
+      print("you must enter a ngative number")
+     else:
+      balance = deposit(balance, amount)
+
+    #withdraw action
+    elif action== "2":
+    
+        amount = int(input("how many do you want to withdraw "))
+        if amount > balance :
+         print("insuufficient  funds")
+        else:
+         balance  = withdraw(balance,amount)
+    #check action
+    elif action == "3" :
+      print("your balance is " + str(balance))
+      #exit action
+    elif action == "exit":
+     break
+
+
+ 
+ 
+
