baa51882f0f5cea365f348272ad0e813c3822ad9
Perryx7 <jacquesperry30@gmail.com>
2025-09-13 00:07:04 +0100

    update of the solution

--- function/basics_ex/b3.py
+++ function/basics_ex/b3.py
@@ -7,10 +7,27 @@
 # Example: { "Alice": 15, "Bob": 12 }
 
 
+def get_menu_choice():
+    """Display menu and get user choice."""
+    print("\n" + "-"*25)
+    print("STUDENT GRADE TRACKER")
+    print("-"*25)
+    print("1. Add a new student")
+    print("2. Update a student's grade")
+    print("3. Display all students")
+    print("4. Exit")
+    print("-"*25)
+
+    
+    choice = input("Choose an option (1-4): ").strip()
+    return choice
 
 
-def add_student ():
-    pass  # TODO: implement add  logic
+
+def add_student (students, student_name, grade_student):
+     students[student_name] = grade_student
+
+     return students
 
 def upgrade_grade():
       pass  # TODO: implement upgrade  logic
@@ -18,12 +35,38 @@ def display_student():
        pass  # TODO: implement display  logic
 def main():
     students = {}
+    print("Welcome to the Student Grade Tracker!")
 
     while True : 
-         student_name = input("enter your name: ")
-         grade_student = input("enter your grade: ")
+         choice = get_menu_choice()
 
-        #  students[student_name] = grade_student
+         if choice == "1" : 
+              
+              student_name = input("Enter a name ")
+              student_grade = int(input("Enter the grade: "))
+              
+              if not student_name : 
+                   print("The name can't be empty")
+              elif student_grade < 0 or student_grade > 20 :
+                   print("Enter a valid grade ")
+              else:
+                   add_student(students, student_name, student_grade)
+                   print(students)
+
+        #  elif choice == "2" :
+
+        #       name_update = input("Enter the name of the students you want to update the grade") 
+
+        #       if name_update not in students : 
+        #            print("the student don't exit ")
+        #       else:
+        #            new_note = int(Enter a new note)
+
+         
+         
+              #add_student(students, student_name, grade_student)
+
+ 
 
 
 
