53ca33259b008bee23b23d1fa05585ba40afa30b
Perryx7 <jacquesperry30@gmail.com>
2025-09-14 23:44:00 +0100

    setup of the main body of the solution

--- function/basics_ex/b5.py
+++ function/basics_ex/b5.py
@@ -6,12 +6,53 @@
 # Delete a contact
 # Show all contacts
 
+def get_menu_choice():
+    """Display menu and get user choice."""
+    print("\n" + "-"*25)
+    print("STUDENT GRADE TRACKER")
+    print("-"*25)
+    print("1. Add a new student")
+    print("2. Update a student's grade")
+    print("3. Display all students")
+    print("4. Exit")
+    print("-"*25)
 
+    
+    choice = input("Choose an option (1-4): ").strip()
+    return choice
 
+def add_con(contacts): 
+    pass #main logic
 
+def del_con(contacts) :
+    pass #main logic
+
+def show_con(contacts) : 
+    pass #main logic
+
+def search_con(contacts) :
+    pass #main logic
 
 def main():
-    pass  # TODO: implement main logic
+   
+   contacts = {}
+
+   print("welcome to your contact book")
+
+   while True : 
+       
+       choice = get_menu_choice
+
+       if choice == "1" : 
+           add_con(contacts)
+       elif choice == "2" :
+           del_con(contacts)
+       elif choice == "3" :
+           search_con(contacts)
+       elif choice == "4" :
+           show_con(contacts)
+       elif choice == "5" :
+           break
 
 
 if __name__ == "__main__":
