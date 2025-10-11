e24f5c70a42d53b22ae61ef421e585b89750ce2b
Perryx7 <jacquesperry30@gmail.com>
2025-10-12 00:37:16 +0100

    best way for handling errors

---
+++ execp_errors/e-e_ex/best_model.py
@@ -0,0 +1,23 @@
+#def your_function(params):
+    #try:
+        # Main logic here
+        #result =  # some operation
+
+    #except SpecificError as e:
+        # Handle specific error
+        #return error_message
+
+    #except AnotherError as e:
+        # Handle another error
+        #return another_message
+
+    #else:
+        # Runs only if NO error
+        # Optional success message
+       # pass
+
+   # finally:
+        # Always runs (cleanup)
+        #pass
+
+    #return result
