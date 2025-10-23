f3af287130ec9bf4054a34ee177b63a1e2eaa946
Perryx7 <jacquesperry30@gmail.com>
2025-10-23 15:14:24 +0100

    set the new docs

--- real_life_ex/POO_4/main.py
+++ real_life_ex/POO_4/main.py
@@ -41,3 +41,23 @@
 # Recommend concerts: “People who attended X also attended Y”.
 
 # Prevent double-booking of the same user for the same concert.
+
+class Registration:
+    def __init__(self, name, email, tickets_bought=False, spending=0):
+        self.name = name
+        self.email = email
+        self.tickets_bought = tickets_bought
+        self.spending = spending
+
+    def user_registration(self):
+        if not self.tickets_bought:
+            self.tickets_bought = True
+            user_info = {
+                'name': self.name,
+                'email': self.email,
+                'spending': self.spending,
+                'tickets_bought': self.tickets_bought
+            }
+            return user_info
+        else:
+            print(f"{self.name} has already been registered.")
